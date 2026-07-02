1a53d373d771dca979c78316ac733e7c076ce9f9
OmarAmir <20202611@csi.edu.eg>
2026-07-03 01:14:37 +0300

    uodated projectcontroller

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,5 +1,10 @@
 from helpers.config import get_settings, Settings
-
+import os
 class BaseController:
     def __init__(self, config: Settings = get_settings()):
+
         self.app_settings = config
+        self.base_dir= os.path.dirname(os.path.dirname(__file__))
+
+        self.files_dir =os.path.join(self.base_dir, "assets", "files")
+        #self.file_dir =os.base_dir + "/" + "assets/files" another way to get the file path but the above way is more robust and platform-independent
