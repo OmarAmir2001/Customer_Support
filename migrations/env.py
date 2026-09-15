deb385271d6b0e651dc9b282f8ce092e6da10e45
OmarAmir2001 <omaramir2001@gmail.com>
2026-09-16 00:51:47 +0300

    feat: Update Docker configuration and Alembic setup for PostgreSQL integration

--- migrations/env.py
+++ migrations/env.py
@@ -9,6 +9,16 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+
+from customer_support.helpers.config import get_settings
+
+settings = get_settings()
+config.set_main_option(
+    "sqlalchemy.url",
+    f"postgresql+psycopg2://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}"
+    f"@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}",
+)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
